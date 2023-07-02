ef75845a95f00d0b6c1eba4c84c9c58609d7dc90
bogdanbooka <booka.friend.of.sun@gmail.com>
2023-07-02 15:21:28 +0300

    Update greek_yaml_to_js.py

--- greek_yaml_to_js.py
+++ greek_yaml_to_js.py
@@ -1,11 +1,4 @@
-import json
-import yaml
-
 
 with open("greek.yaml", "r") as stream:
-    yaml_content = yaml.safe_load(stream)
-
-json_str = json.dumps(yaml_content)
-
-with open('greek.js', 'w') as f:
-    f.write(f'const en_greek_ru = {json.dumps(yaml_content, indent=4)}')
+    with open('greek.js', 'w') as f:
+        f.write(f'const greek_yaml = `\n{stream.read()}`')
