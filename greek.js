3a604590072b495607b190919b8c20d1988f4900
bogdanbooka <booka.friend.of.sun@gmail.com>
2023-07-16 02:00:06 +0300

    Update greek.js

--- greek.js
+++ greek.js
@@ -11,15 +11,182 @@ a1:
     gr: γεια
     ru: привет
   - en: I am <name>
-    gr: Είμαι ο/η <ονομα>
+    gr: Είμαι ο/η <όνομα>
     ru: Я <имя>
   - en: name
-    gr: ονομα
+    gr: όνομα
     ru: имя
   - en: second name
     gr: επώνυμο
     ru: фамилия
-
+  - gr: εγώ
+    en: I
+    ru: я
+  - gr: εσύ
+    en: you (friend)
+    ru: ты
+  - gr: αυτός
+    en: he
+    ru: он
+  - gr: αυτή
+    en: she
+    ru: она
+  - gr: αυτό
+    en: it
+    ru: это
+  - gr: εμείς
+    en: we
+    ru: мы
+  - gr: εσείς
+    en: you (polite)
+    ru: вы
+  - gr: αυτοί
+    en: they
+    ru: они (мр/мр+жр)
+  - gr: αυτοί
+    en: they
+    ru: они (жр)
+  - gr: αυτά
+    en: they
+    ru: они (ср)
+  - gr: είμαι
+    en: i am
+    ru: я есть
+  - gr: είσαι
+    en: you are
+    ru: ты есть
+  - gr: είναι
+    en: he/she/it is (they are)
+    ru: он/она/оно/они есть
+  - gr: είμαστε
+    en: we are
+    ru: мы есть
+  - gr: είστε / είσαστε
+    en: you are (polite)
+    ru: вы есть
+  - gr: Από 'δώ ο/η <όνομα>
+    en: <Name> is here / Here is <Name>
+    ru: Здесь <имя> / Это <имя>.
+  - gr: Πώς σε λένε;
+    en: What is your name? (friend)
+    ru: Как тебя зовут?
+  - gr: Πώς σας λένε;
+    en: What is your name? (polite)
+    ru: Как вас зовут?
+  - gr: Είμαι απο την Ρωσία
+    en: I am from Russia
+    ru: Я из России
+  - gr: Εδώ είμαστε
+    en: We are here
+    ru: Мы здесь
+  - gr: Εσύ είναι η Άννα;
+    en: Are you Anna?
+    ru: Ты Анна?
+  - gr: όχι
+    en: not
+    ru: нет
+  - gr: όχι, δεν είμαι η Άννα
+    en: No, I am not Anna
+    ru: нет, я не Анна
+  - gr: τώρα
+    en: now
+    ru: сейчас
+  - gr: μου
+    en: my
+    ru: мой
+  - gr: μιλάω
+    en: i speak
+    ru: я говорю
+  - gr: κάποιον
+    en: someone
+    ru: кто-то
+  - gr: όπος
+    en: where
+    ru: где
+  - gr: παράδειγμα
+    en: example
+    ru: пример
+  - gr: πάλι
+    en: again
+    ru: опять
+  - gr: Βάζω
+    en: I put
+    ru: я кладу
+  - gr: όλος
+    en: all
+    ru: все
+  - gr: Διαβάζω
+    en: I'm studying / I am reading
+    ru: я учусь / я читаю
+  - gr: Γράφω
+    en: I am writing
+    ru: Я пишу
+  - gr: Πότε
+    en: When
+    ru: Когда
+  - gr: Χαίρω πολύ
+    en: Nice to meet you
+    ru: Рад встрече
+  - gr: Από πού είσαι; 
+    en: Where are you from?
+    ru: Откуда ты?
+  - gr: μάθημα
+    en: lesson
+    ru: урок
+  - gr: Πώς λέγεστε
+    en: What is your name? (polite)
+    ru: Как вас зовут?
+  - gr: Ευχαριστώ
+    en: Thanks
+    ru: Спасибо
+  - gr: σωστό
+    en: correct / right
+    ru: верно / правильно
+  - gr: μηδέν 
+    en: 0
+    ru: ноль
+  - gr: ένα
+    en: 1
+    ru: 1
+  - gr: δύο
+    en: 2
+    ru: 2
+  - gr: τρία
+    en: 3
+    ru: 3
+  - gr: τέσσερα
+    en: 4
+    ru: 4
+  - gr: πέντε
+    en: 5
+    ru: 5
+  - gr: έξι
+    en: 6
+    ru: 6
+  - gr: εφτά / επτά
+    en: 7
+    ru: 7
+  - gr: οχτώ / οκτώ
+    en: 8
+    ru: 8
+  - gr: εννιά / εννέα
+    en: 9
+    ru: 9
+  - gr: δέκα
+    en: 10
+    ru: 10
+  - gr: συμπληρώνω
+    en: Fill in
+    ru: заполнить
+  - gr: φτιάχνω
+    en: I make
+    ru: я делаю (make)
+  - gr: Πως
+    en: How 
+    ru: Как
+  - gr: Τι
+    en: What
+    ru: Что
 
 a2:
   - en: How are you?
@@ -28,6 +195,138 @@ a2:
   - en: where do you live?
     gr: που μένεις;
     ru: где живешь?
+  - gr: Έχεις τηλέφωνο;
+    en: Do you have a phone?
+    ru: У тебя есть телефон?
+  - gr: Το τηλέφωνό μου είναι <...>
+    en: Мой телефон <...>
+    ru: My phone is <...>
+  - gr: κάνω
+    en: I am doing
+    ru: я делаю (+ склонения)
+  - gr: Πώς είσαι;
+    en: How are you?
+    ru: Как ты?
+  - gr: Μια χαρά
+    en: I'm fine
+    ru: Я - отлично!
+  - gr: Πού ακριβώς;
+    en: Where exactly?
+    ru: Где именно?
+  - gr: Μένεις εδώ κοντά;
+    en: Do you live nearby?
+    ru: Вы живете поблизости?
+  - gr: κοντά
+    en: near
+    ru: рядом
+  - gr: στον δρόμο
+    en: on the road
+    ru: на дороге
+  - gr: καλημέρα
+    en: good morning
+    ru: доброе утро
+  - gr: καλησπέρα
+    en: Добрый вечер
+    ru: Good Evening
+  - gr: Τα λέμε
+    en: See you
+    ru: Увидимся
+  - gr: Καλό βράδυ
+    en: Good night
+    ru: Хорошего вечера
+  - gr: Καληνύχτα
+    en: Good night
+    ru: Спокойной ночи
+  - gr: Χαίρετε
+    en: Greetings
+    ru: Have fun
+  - gr: Αντίο
+    en: Goodbye
+    ru: До свидания
+  - gr: Πολύ καλά
+    en: Very well
+    ru: Очень хорошо
+  - gr: Καλά
+    en: Good
+    ru: Хорошо
+  - gr: Έτσι κι έτσι
+    en: So so
+    ru: Так себе
+  - gr: Τα ίδια
+    en: The same
+    ru: Также
+  - gr: Χάλια
+    en: Sucks
+    ru: Отстой
+  - gr: όχι και τόσο καλά
+    en: Not so good
+    ru: Не так хорошо, как хотелось бы
+  - gr: Καλούτσικα
+    en: Not so bad
+    ru: Не так плохо
+  - gr: έντεκα δώδεκα
+    en: 11 12
+    ru: 11 12
+  - gr: δεκατρία - δεκαεννιά
+    en: 13 - 19
+    ru: 13 - 19
+  - gr: είκοσι - εκατό
+    en: 20 - 100
+    ru: 20 - 100
+  - gr: μένω
+    en: stay
+    ru: оставаться / жить (склонения)
+  - gr: Ξέρω
+    en: I know
+    ru: Я знаю
+  - gr: Χώρα
+    en: Country
+    ru: Страна
+  - gr: κινητό
+    en: mobile
+    ru: мобильный
+  - gr: σταθερό
+    en: stationary
+    ru: стационарный
+  - gr: εχώ
+    en: I have
+    ru: имею (склонения)
+  - gr: μου
+    en: my
+    ru: мой
+  - gr: σου
+    en: your
+    ru: твой
+  - gr: του
+    en: his its
+    ru: его этого
+  - gr: της
+    en: her
+    ru: её
+  - gr: μας
+    en: our
+    ru: наш
+  - gr: σας
+    en: your
+    ru: ваш
+  - gr: τους
+    en: their
+    ru: их
+  - gr: διεύθυνση
+    en: address
+    ru: адрес
+  - gr: πόλη
+    en: city
+    ru: город
+  - gr: περιοχή
+    en: region
+    ru: область
+  - gr: ταχυδρομικός κώδικος
+    en: Postal code
+    ru: Почтовый индекс
+  - gr: υπογραφή
+    en: signature
+    ru: подпись
 
 a3:
   - en: What do you do?
@@ -275,6 +574,7 @@ a6:
   - gr: μήνας
     en: month
     ru: месяц
+
 a7:
   - gr: ποτέ
     en: never
